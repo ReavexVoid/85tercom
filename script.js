0c096df360e5c5921c7ca792be33062c93e31ca6
ReavexVoid <reavexfromvoid6@gmail.com>
2026-05-16 13:14:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let mode = "";
+
+const sounds = {
+    scp: [
+        "alarm.mp3",
+        "breach.mp3"
+    ],
+    mtf: [
+        "hammerdownnu7entry.mp3",
+        "mazhatbeta7entry.mp3",
+        "ntfepsilon11entry.mp3",
+        "rrhalpha1entry.mp3"
+    ]
+};
+
+function setMode(selected) {
+    mode = selected;
+
+    document.getElementById("modeTitle").innerText =
+        selected.toUpperCase() + " INTERCOM PANEL";
+
+    renderList();
+}
+
+function renderList() {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    sounds[mode].forEach(file => {
+        const btn = document.createElement("button");
+        btn.innerText = file;
+
+        btn.onclick = () => playSound(file);
+
+        list.appendChild(btn);
+    });
+}
+
+function playSound(file) {
+    const player = document.getElementById("player");
+    player.src = `audio/${mode}/${file}`;
+    player.play();
+}
